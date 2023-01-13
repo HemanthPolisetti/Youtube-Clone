a2e981c42c64dfda09a76b7342cea24fb2438730
Captain_Levi <hemanthpolisetti@gmail.com>
2023-01-13 05:44:40 +0000

    ms

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
+import {Box} from '@mui/material';
+import Feed from './Components/Feed';
+import VideoDetails from './Components/VideoDetails';
+import ChannelDetails from './Components/ChannelDetails';
+import SearchFeed from './Components/SearchFeed';
+import Navbar from './Components/Navbar';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Box sx={{backgroundColor:'#000'}}>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetails />} />
+        <Route path='/channel/:id' element={<ChannelDetails />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />}/>
+
+      </Routes>
+    </Box>
+    </BrowserRouter>
+  );
+}
+
+export default App;
